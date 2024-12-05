8bfac91b1cbcfa6732c3e00126fcf00cc2202d3e
Fold1 <f0ld1@mail.ru>
2024-12-05 09:21:48 +0700

    add src and modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
+import getDate from "./src/index.js"
 
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+getDate()
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import _ from "lodash"
+
+export default () => {
+
+	console.log(_.now())
+
+}
